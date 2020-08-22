b8903c26bdafa880bab387f2c2ff395166cc7db4
Robert Stefanic <rstefanic72@gmail.com>
2020-08-22 18:01:54 -0400

    Added TypeScript and Webpack; removed react-scripts
    Added TypeScript and Webpack. Created tsconfig.json for TypeScript options and added webpack.config.js to bundle the project. Also added src/index.html
    and added to webpack to display our output.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: './main.js'
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".css"],
+    },
+    module: {
+        rules: [
+            { test: /\.(t|j)sx?$/, use: { loader: 'ts-loader' }, exclude: /node_modules/ },
+            { enforce: "pre", test: /\.js$/, exclude: /node_modules/, loader: "source-map-loader" },
+            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]},
+            { test: /\.html$/, use: [ { loader: 'html-loader' }]}
+        ]
+    },
+    mode: "development",
+    devtool: "source-map",
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
